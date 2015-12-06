6741a021c3be852712e2d8e7964a3759377c1d2f
Jani Mäkelä <jani.makela@pp3.inet.fi>
2015-12-06 15:48:12 +0200

    Solved both day 1 problems in haskell.

--- haskell/problem011.hs
+++ haskell/problem011.hs
@@ -1,1 +1,12 @@
+solve :: String -> Int
+solve [] = 0
+solve (x:xs)
+	| x == '(' = (solve xs) + 1
+	| x == ')' = (solve xs) - 1
+	| otherwise = error "invalid input for solve."
 
+
+main = do
+	file <- readFile "../inputs/problem011.txt"
+	let input = (lines file) !! 0
+	return (solve input)
---
+++ haskell/problem012.hs
@@ -0,0 +1,13 @@
+solve :: String -> Int -> Int -> Int
+solve [] _ _ = error "Basement not found"
+solve (x:xs) i sum
+	| sum == -1 = i
+	| x == '(' = solve xs (i + 1) (sum + 1)
+	| x == ')' = solve xs (i + 1) (sum - 1)
+	| otherwise = error "invalid input for solve."
+
+
+main = do
+	file <- readFile "../inputs/problem011.txt"
+	let input = (lines file) !! 0
+	return (solve input 0 0)
