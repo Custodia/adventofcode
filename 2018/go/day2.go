19809d33ff006893e9a184c65a86b0d0cb555d43
Jani Mäkelä <janimakelaa@gmail.com>
2018-12-10 19:51:23 +0200

    Solve day 2 challenges with go

---
+++ 2018/go/day2.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"sort"
+	. "adventofcode2018/helper"
+)
+
+const fileName string = "../inputs/day2.txt"
+
+func main() {
+	result1 := solvePart1()
+	fmt.Printf("Result 1: %d\n", result1)
+	result2 := solvePart2()
+	fmt.Printf("Result 2: %s\n", result2)
+}
+
+func solvePart1() int {
+	count2, count3 := 0, 0
+	file, scanner := NewScanner(fileName)
+	defer file.Close()
+
+	for scanner.Scan() {
+		m := make(map[rune]int)
+		twos, threes := false, false
+		text := scanner.Text()
+		for _, rune := range text {
+			m[rune] = m[rune] + 1
+		}
+		for _, value := range m {
+			twos = twos || value == 2
+			threes = threes || value == 3
+			if twos && threes {
+				break
+			}
+		}
+		if twos {
+			count2 += 1
+		}
+		if threes {
+			count3 += 1
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return count2 * count3
+}
+
+func solvePart2() string {
+	lineCount := CountLines(fileName)
+	file, scanner := NewScanner(fileName)
+	defer file.Close()
+
+	slice := make([]string, lineCount)
+	i := 0
+	for scanner.Scan() {
+		slice[i] = scanner.Text()
+		i++
+	}
+
+	sort.Strings(slice)
+	i = 0
+	result := ""
+	for i, str := range slice {
+		nextStr := []rune(slice[i + 1])
+		commonChars := []rune{}
+		for ii, rune := range str {
+			if rune == nextStr[ii] {
+				commonChars = append(commonChars, rune)
+			}
+		}
+		if len(commonChars) + 1 == len(str) {
+			result = string(commonChars)
+			break
+		}
+		i++
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return result
+}
--- 2018/go/src/adventofcode2018/helper/helper.go
+++ 2018/go/src/adventofcode2018/helper/helper.go
@@ -1,7 +1,9 @@
 package helper
 
 import (
+	"io"
 	"os"
+	"bytes"
 	"log"
 	"bufio"
 )
@@ -15,4 +17,29 @@ func NewScanner(filename string) (*os.File, *bufio.Scanner) {
 	scanner := bufio.NewScanner(file)
 
 	return file, scanner
+}
+
+func CountLines(filename string) int {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	buf := make([]byte, 32*1024)
+	count := 0
+	lineSep := []byte{'\n'}
+
+	for {
+		c, err := file.Read(buf)
+		count += bytes.Count(buf[:c], lineSep)
+
+		switch {
+		case err == io.EOF:
+			return count
+
+		case err != nil:
+			log.Fatal(err)
+		}
+	}
 }
