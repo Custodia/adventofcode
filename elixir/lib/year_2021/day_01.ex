3c5311ebf52fb9eb1c81473c85ecf63ff7e3897a
Jani Mäkelä <jmakela@synack.com>
2021-12-01 21:55:53 +0200

    Add solutions for day 1

---
+++ elixir/lib/year_2021/day_01.ex
@@ -0,0 +1,31 @@
+defmodule AdventOfCode.Year2021.Day01 do
+  @filename "../2021/inputs/day01.txt"
+
+  def part1 do
+    result = File.stream!(@filename)
+    |> Stream.map(&String.trim/1)
+    |> Stream.map(&String.to_integer/1)
+    |> Enum.reduce({-1, -1}, fn e, {prev, count} ->
+      if e > prev, do: {e, count + 1}, else: {e, count}
+    end)
+    |> then(&elem(&1, 1))
+    result
+  end
+
+  def part2 do
+    result = File.stream!(@filename)
+    |> Stream.map(&String.trim/1)
+    |> Enum.map(&String.to_integer/1)
+    |> then(&count_sliding_windows/1)
+    |> Enum.reduce({-1, -1}, fn e, {prev, count} ->
+      if e > prev, do: {e, count + 1}, else: {e, count}
+    end)
+    |> then(&elem(&1, 1))
+    result
+  end
+
+  defp count_sliding_windows([first, second | tail]) do
+    Enum.map_reduce(tail, {first, second}, fn e, {f, s} -> {e + f + s, {e, f}} end)
+    |> then(&elem(&1, 0))
+  end
+end
---
+++ elixir/test/year_2021/day_01_test.exs
@@ -0,0 +1,11 @@
+defmodule AdventOfCode.Year2021.Day01Test do
+  use ExUnit.Case
+
+  test "part1" do
+    assert AdventOfCode.Year2021.Day01.part1 == 1832
+  end
+
+  test "part2" do
+    assert AdventOfCode.Year2021.Day01.part2 == 1858
+  end
+end
