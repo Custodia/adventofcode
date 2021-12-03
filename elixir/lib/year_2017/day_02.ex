418f8b93aedf05dd497ba902da6decac9319eba9
Jani Mäkelä <jmakela@synack.com>
2021-12-03 19:54:08 +0200

    Add solution for year 2017 day 2

---
+++ elixir/lib/year_2017/day_02.ex
@@ -0,0 +1,27 @@
+defmodule AdventOfCode.Year2017.Day02 do
+  @filename "../2017/inputs/day02.txt"
+
+  def part1 do
+    File.stream!(@filename)
+    |> Stream.map(&String.trim/1)
+    |> Stream.map(&String.split(&1, "\t"))
+    |> Stream.map(&Enum.map(&1, fn e -> String.to_integer(e) end))
+    |> Stream.map(&Enum.min_max/1)
+    |> Stream.map(fn {min, max} -> max - min end)
+    |> Enum.sum()
+  end
+
+  def part2 do
+    File.stream!(@filename)
+    |> Stream.map(&String.trim/1)
+    |> Stream.map(&String.split(&1, "\t"))
+    |> Stream.map(&Enum.map(&1, fn e -> String.to_integer(e) end))
+    |> Stream.map(&get_evenly_divisible(&1, &1))
+    |> Stream.map(fn [result] -> result end)
+    |> Enum.sum()
+  end
+
+  def get_evenly_divisible(arr1, arr2) do
+    for x <- arr1, y <- arr2, x != y && rem(x, y) == 0, do: div(x, y)
+  end
+end
---
+++ elixir/test/year_2017/day_02_test.exs
@@ -0,0 +1,12 @@
+defmodule AdventOfCode.Year2017.Day02Test do
+  use ExUnit.Case
+  alias AdventOfCode.Year2017
+
+  test "part1" do
+    assert Year2017.Day02.part1 == 42299
+  end
+
+  test "part2" do
+    assert Year2017.Day02.part2 == 277
+  end
+end
