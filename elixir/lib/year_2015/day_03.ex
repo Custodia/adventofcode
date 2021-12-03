1d645826840575fd945fbd8462dfddcce2dd8be2
Jani Mäkelä <jmakela@synack.com>
2021-12-03 11:57:37 +0200

    Add solution for 2015 day 3

---
+++ elixir/lib/year_2015/day_03.ex
@@ -0,0 +1,41 @@
+defmodule AdventOfCode.Year2015.Day03 do
+  @filename "../2015/inputs/day03.txt"
+
+  def part1 do
+    File.stream!(@filename, [], 1)
+    |> Stream.filter(fn c -> c in ["^", ">", "v", "<"] end)
+    |> Enum.scan({0, 0}, &calculate_new_location/2)
+    |> then(&([{0, 0} | &1]))
+    |> Enum.uniq()
+    |> Enum.count()
+  end
+
+  defp calculate_new_location(direction, {x, y}) do
+    case direction do
+      "^" -> {x, y + 1}
+      "v" -> {x, y - 1}
+      ">" -> {x + 1, y}
+      "<" -> {x - 1, y}
+    end
+  end
+
+  def part2 do
+    santa = File.stream!(@filename, [], 1)
+    |> Stream.filter(fn c -> c in ["^", ">", "v", "<"] end)
+    |> Stream.with_index()
+    |> Stream.filter(fn {_, i} -> rem(i, 2) == 0 end)
+    |> Stream.map(&elem(&1, 0))
+    |> Enum.scan({0, 0}, &calculate_new_location/2)
+
+    robo_santa = File.stream!(@filename, [], 1)
+    |> Stream.filter(fn c -> c in ["^", ">", "v", "<"] end)
+    |> Stream.with_index()
+    |> Stream.filter(fn {_, i} -> rem(i, 2) == 1 end)
+    |> Stream.map(&elem(&1, 0))
+    |> Enum.scan({0, 0}, &calculate_new_location/2)
+
+    Enum.concat([[{0, 0}], santa, robo_santa])
+    |> Enum.uniq()
+    |> Enum.count()
+  end
+end
---
+++ elixir/test/year_2015/day_03_test.exs
@@ -0,0 +1,11 @@
+defmodule AdventOfCode.Year2015.Day03Test do
+  use ExUnit.Case
+
+  test "part1" do
+    assert AdventOfCode.Year2015.Day03.part1 == 2572
+  end
+
+  test "part2" do
+    assert AdventOfCode.Year2015.Day03.part2 == 2631
+  end
+end
