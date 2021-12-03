67bf09fa3df4b0961f941aa9e2f43cd5ab338524
Jani Mäkelä <jmakela@synack.com>
2021-12-03 11:30:14 +0200

    Add solution for 2015 day 2

---
+++ elixir/lib/year_2015/day_02.ex
@@ -0,0 +1,29 @@
+defmodule AdventOfCode.Year2015.Day02 do
+  @filename "../2015/inputs/day02.txt"
+
+  def part1 do
+    File.stream!(@filename)
+    |> then(&parse_input_file_stream/1)
+    |> Stream.map(fn [x, y, z] -> [x*y, x*z, y*z] end)
+    |> Stream.map(fn [a, b, c] -> 2*a + 2*b + 2*c + Enum.min([a, b, c]) end)
+    |> Enum.sum()
+  end
+
+  def part2 do
+    File.stream!(@filename)
+    |> then(&parse_input_file_stream/1)
+    |> Stream.map(fn dimensions ->
+      circumerence = Enum.sort(dimensions) |> Enum.take(2) |> Enum.map(&(&1 * 2)) |> Enum.sum
+      ribbon = Enum.product(dimensions)
+      circumerence + ribbon
+    end)
+    |> Enum.sum()
+  end
+
+  defp parse_input_file_stream(file_stream) do
+    file_stream
+    |> Stream.map(&String.trim/1)
+    |> Stream.map(&String.split(&1, "x"))
+    |> Stream.map(&Enum.map(&1, fn e -> String.to_integer(e) end))
+  end
+end
---
+++ elixir/test/year_2015/day_02_test.exs
@@ -0,0 +1,11 @@
+defmodule AdventOfCode.Year2015.Day02Test do
+  use ExUnit.Case
+
+  test "part1" do
+    assert AdventOfCode.Year2015.Day02.part1 == 1598415
+  end
+
+  test "part2" do
+    assert AdventOfCode.Year2015.Day02.part2 == 3812909
+  end
+end
