c98d54dc45916e177e7b138d494be605e9b03473
Jani Mäkelä <jmakela@synack.com>
2021-12-06 21:14:20 +0200

    Add solution for 2018 day 1

---
+++ elixir/lib/year_2018/day_01.ex
@@ -0,0 +1,25 @@
+defmodule AdventOfCode.Year2018.Day01 do
+  @filename "../2018/inputs/day01.txt"
+
+  def part1 do
+    File.stream!(@filename)
+    |> Stream.map(&String.trim/1)
+    |> Stream.map(&String.to_integer/1)
+    |> Enum.sum()
+  end
+
+  def part2 do
+    File.stream!(@filename)
+    |> Stream.map(&String.trim/1)
+    |> Stream.map(&String.to_integer/1)
+    |> Stream.cycle()
+    |> Stream.scan(&(&1 + &2))
+    |> Enum.reduce_while(MapSet.new(), fn e, acc ->
+      if MapSet.member?(acc, e) do
+        {:halt, e}
+      else
+        {:cont, MapSet.put(acc, e)}
+      end
+    end)
+  end
+end
---
+++ elixir/test/year_2018/day_01_test.exs
@@ -0,0 +1,12 @@
+defmodule AdventOfCode.Year2018.Day01Test do
+  use ExUnit.Case
+  alias AdventOfCode.Year2018
+
+  test "part1" do
+    assert Year2018.Day01.part1 == 497
+  end
+
+  test "part2" do
+    assert Year2018.Day01.part2 == 558
+  end
+end
