d3627d804e05cede7d1deaefba2cbdc83f891a9e
Jani Mäkelä <jmakela@synack.com>
2021-12-06 20:43:35 +0200

    Refactor 2020 day 1 solution to be performant

--- elixir/lib/year_2020/day_01.ex
+++ elixir/lib/year_2020/day_01.ex
@@ -18,18 +18,30 @@ defmodule AdventOfCode.Year2020.Day01 do
   def find_two_that_sum(numbers, target \\ 2020) do
     combinations = for x <- numbers, y <- numbers, x != y, do: {x, y}
 
-    Enum.find(combinations, fn {x, y} -> x + y == 2020 end)
+    Enum.find(combinations, fn {x, y} -> x + y == target end)
   end
+  def find_two_that_sum([], [], _target), do: nil
+  def find_two_that_sum([_], _, _target), do: nil
+  def find_two_that_sum([_a1, a2 | atail], [], target), do:
+    find_two_that_sum([a2 | atail], atail, target)
+  def find_two_that_sum([a | _], [b | _], target) when a + b == target, do:
+    {a, b}
+  def find_two_that_sum([a | atail], [b | btail], target) when a + b > target, do:
+    find_two_that_sum([a | atail], btail, target)
+  def find_two_that_sum([_a1, a2 | atail], _, target), do:
+    find_two_that_sum([a2 | atail], atail, target)
 
-  def find_three_that_sum(numbers, target \\ 2020) do
-    combinations = for x <- numbers, y <- numbers, z <- numbers, x != y and x != z and y != z, do: {x, y, z}
-
-    Enum.find(combinations, fn {x, y, z} -> x + y + z == 2020 end)
+  defp find_three_that_sum([n | number_tail], target \\ 2020) do
+    case find_two_that_sum(number_tail, target - n) do
+      nil -> find_three_that_sum(number_tail, target)
+      {x, y} -> {x, y, n}
+    end
   end
 
   def parse_input(stream) do
     stream
     |> Stream.map(&String.trim/1)
-    |> Enum.map(&String.to_integer/1)
+    |> Stream.map(&String.to_integer/1)
+    |> Enum.sort(&>=/2)
   end
 end
