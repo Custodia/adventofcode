598a51921b129ed6bd06ade078425b2d605522f4
Jani Mäkelä <jmakela@synack.com>
2021-12-07 10:40:28 +0200

    Add solution for 2021 day 7

---
+++ elixir/lib/year_2021/day_07.ex
@@ -0,0 +1,49 @@
+defmodule AdventOfCode.Year2021.Day07 do
+  @filename "../2021/inputs/day07.txt"
+
+  def part1 do
+    numbers = File.read!(@filename) |> parse_input()
+    {min, max} = Enum.min_max(numbers)
+
+    min..max
+    |> Enum.map(fn x ->
+      distance = Enum.reduce(numbers, 0, fn y, acc -> abs(x - y) + acc end)
+      {x, distance}
+    end)
+    |> Enum.min_by(fn {_, distance} -> distance end)
+    |> then(fn {_, distance} -> distance end)
+  end
+
+  def part2 do
+    numbers = File.read!(@filename) |> parse_input()
+    {min, max} = Enum.min_max(numbers)
+    crab_distances =
+      min..max
+      |> Stream.with_index()
+      |> Enum.scan({0, 0}, fn {_, i}, {_, acc} -> {i, acc + i} end)
+      |> Map.new()
+
+    min..max
+    |> Enum.map(fn x ->
+      distance = Enum.reduce(numbers, 0, fn y, acc ->
+        Map.get(crab_distances, abs(x - y)) + acc
+      end)
+      {x, distance}
+    end)
+    |> Enum.min_by(fn {_, distance} -> distance end)
+    |> then(fn {_, distance} -> distance end)
+  end
+
+  def crab_distance(x, y) do
+    x..y
+    |> Stream.scan(0, fn _, acc -> acc + 1 end)
+    |> Enum.sum()
+  end
+
+  def parse_input(input) do
+    input
+    |> String.trim()
+    |> String.split(",")
+    |> Enum.map(&String.to_integer/1)
+  end
+end
---
+++ elixir/test/year_2021/day_07_test.exs
@@ -0,0 +1,12 @@
+defmodule AdventOfCode.Year2021.Day07Test do
+  use ExUnit.Case
+  alias AdventOfCode.Year2021
+
+  test "part1" do
+    assert Year2021.Day07.part1 == 344138
+  end
+
+  test "part2" do
+    assert Year2021.Day07.part2 == 94862124
+  end
+end
